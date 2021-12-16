f5cb5da58b21dd0019ff7a7c580cd6e2c8e8c6fe
Joao Mario <joaomf17p2@gmail.com>
2021-12-16 17:11:07 -0300

    commit

--- src/components/reusable/NavBar.vue
+++ src/components/reusable/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="nav-bar">
-    NavBar
+    <img src="../../assets/icons/home_logo.svg" alt="">
+    <div class="nav-bar-search-bar"></div>
+    <div ></div>
   </div>
 </template>
 
@@ -10,10 +12,22 @@
 
 <style scoped>
   .nav-bar{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
     margin: 0;
     padding: 0;
     width: 100%;
 
     background-color: black;
   }
+  .nav-bar-search-bar{
+    width: 100px;
+    height: 10px;
+
+    background-color: white;
+  }
+
 </style>
