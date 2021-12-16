cc83b49f754e85c3fe1e0bd000a913e4f54f32a2
regiszaum <regisf97@gmail.com>
2021-12-16 17:01:34 -0300

    imagens

---
+++ src/components/screens/TelaLogin.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+* {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+</style>
