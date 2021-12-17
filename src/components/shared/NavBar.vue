71d07f00919b3702e34db997e5d4ebd1397d010d
Joao Mario <joaomf17p2@gmail.com>
2021-12-17 09:24:26 -0300

    nav bar

--- src/components/shared/NavBar.vue
+++ src/components/shared/NavBar.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="nav-bar">
-    <img id="home_icon" src="../../assets/icons/home_logo.svg" alt="">
-    <div class="nav-bar-search-bar">
-      <img id="soluti_logo" src="../../assets/images/logo.png" alt="">
+    <div class="item_1">
+      <div class="nav-bar-home-button">
+        <img  id="home_icon" src="../../assets/icons/home_logo.svg" alt="">
+
+        <hr id="hover-line">
+      </div>
+       
     </div>
-    <div class="nav-bar-right-content">
+
+    <div class="item_2">
+      <div class="nav-bar-search-bar">
+        <img id="soluti_logo" src="../../assets/images/logo.png" alt="soluti_logo">
+        <img id="search_icon" src="../../assets/icons/search_icon.svg" alt="search_icon">
+      </div>
+    </div>
+
+    
+    <div class="item_3">
       <div class="nav-bar-user-tag"></div>
-      <img class="nav-bar-right-content_icon" src="../../assets/icons/messeger_icon.svg" alt="">
-      <img class="nav-bar-right-content_icon" src="../../assets/icons/notifications_icon.svg" alt="">
-      <img class="nav-bar-right-content_icon" src="../../assets/icons/configuration_icon.svg" alt="">
+      <img id="message_icon" src="../../assets/icons/messeger_icon.svg" alt="">
+      <img id="notification_icon" src="../../assets/icons/notifications_icon.svg" alt="">
+      <img id="config_icon" src="../../assets/icons/configuration_icon.svg" alt="">
     </div>
   </div>
 </template>
@@ -19,8 +32,11 @@
 
 <style scoped>
   .nav-bar {
+    position: fixed;
+
     display: grid;
     grid-template-columns: 30% 40% 30%;
+    grid-template-areas: "A B C";
     justify-items: center;
     align-content: center;
   
@@ -33,13 +49,52 @@
     background-color: var(--verde-escuro);
   }
 
-  #home_icon{
-    align-self: flex-start;
-  }
+  .nav-bar > * {
+    width: 100%;
 
-  .nav-bar-search-bar {
     display: flex;
     align-items: center;
+    justify-content: center;
+  }
+
+  .item_1{
+    justify-content: flex-start;
+  }
+
+  .item_1 .nav-bar-home-button{
+    margin-left: 50px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .item_1 .nav-bar-home-button:hover #hover-line{
+    display: inline;
+  }
+
+  .item_1 .nav-bar-home-button #home_icon{
+    align-self: center;
+
+    width: auto;
+    height: 25px;
+  }
+
+  .item_1 .nav-bar-home-button #hover-line{
+    position: absolute;
+    top: 64px;
+
+    width: 15px;
+    border: 3px solid white;
+
+    display: none;
+  }
+
+  .item_2 .nav-bar-search-bar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
     width: 400px;
     height: 37px;
@@ -49,22 +104,17 @@
     background-color: white;
   }
 
-  #soluti_logo{
+  .item_2 #soluti_logo{
     margin-left: -7px;
     width: auto;
     height: 50px;
   }
 
-  .nav-bar-right-content{
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-
-    width: 350px;
-    height: 37px;
+  .item_2 #search_icon{
+    margin-right: 3px;
   }
 
-  .nav-bar-right-content .nav-bar-user-tag {
+  .item_3 .nav-bar-user-tag {
     width: 100px;
     height: 37px;
     margin-right: 20px;
@@ -72,6 +122,23 @@
     border-radius: 10px;
 
     background-color: var(--branco);
+  }
+
+  .item_3 > img{
+    margin-left: 20px;
+    width: auto;
+  }
+
+  .item_3 #message_icon{
+    height: 23px;
+  }
+
+  .item_3 #notification_icon{
+    height: 26px;
+  }
+
+  .item_3 #config_icon{
+    height: 23px;
   }
 
   .nav-bar-right-content .nav-bar-right-content_icon{
