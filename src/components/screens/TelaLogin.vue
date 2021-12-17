0c5bb7aaa6ca037598602934aad1bae1e7bf5fcf
regiszaum <regisf97@gmail.com>
2021-12-17 12:06:11 -0300

    erro de merge?

--- src/components/screens/TelaLogin.vue
+++ src/components/screens/TelaLogin.vue
@@ -5,31 +5,16 @@
         </div>
         <div class="form-login">
             <h1>Login</h1>
-<<<<<<< HEAD
             <input type="text" name="username" placeholder="Username" />
             <input type="password" name="password" placeholder="Password" />
-=======
-            <!-- v-model="input.username" -->
-            <input type="text" name="username"  placeholder="Username" />
-            <!-- v-model="input.password" -->
-            <input type="password" name="password"  placeholder="Password" />
->>>>>>> c0863e9a35105a7002b29d27cd8e0b70b33c078b
             <button type="button">Login</button>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-<<<<<<< HEAD
-    name: 'Login',
-
-=======
+export default { 
     name: 'TelaLogin',
-    /* components: {
-        TelaLogin
-    } */
->>>>>>> c0863e9a35105a7002b29d27cd8e0b70b33c078b
 }
 </script>
 
