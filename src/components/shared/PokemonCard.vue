bcaa7b0fedaa438a5cb848d2975de61c6d054866
regiszaum <regisf97@gmail.com>
2022-01-06 15:31:46 -0300

    loading cards

--- src/components/shared/PokemonCard.vue
+++ src/components/shared/PokemonCard.vue
@@ -1,335 +1,431 @@
 <template>
-  <div v-if="isFetched" class="back-card-line ">
-    <div :class="['card-fill',pokemon.types[0].type.name]">
+  <div v-if="isFetched" class="back-card-line">
+    <div :class="['card-fill', pokemon.types[0].type.name]">
       <div class="name-n-power">
-        <h1>{{pokemonNameUpperCase(pokemon.name)}}</h1>
+        <h1>{{ pokemonNameUpperCase(pokemon.name) }}</h1>
         <div class="powers">
-          <img draggable="false" v-for="element in pokemon.types" :key="element.slot" id="type-icon" :src="require(`../../assets/type-icons/${element.type.name}.svg`)">
-          <h2>{{pokemon.stats.hp + " HP"}}</h2>
-      </div>
+          <img
+            draggable="false"
+            v-for="element in pokemon.types"
+            :key="element.slot"
+            id="type-icon"
+            :src="require(`../../assets/type-icons/${element.type.name}.svg`)"
+          />
+          <h2>{{ pokemon.stats.hp + " HP" }}</h2>
+        </div>
       </div>
 
       <div class="pokemon-display-back-line">
-        <div class="pokemon-display"> 
-          <img draggable="false" :src="pokemon.img" :alt="pokemon.nome">
+        <div class="pokemon-display">
+          <img draggable="false" :src="pokemon.img" :alt="pokemon.nome" />
         </div>
       </div>
 
       <div class="pokemon-stats">
         <div class="stat">
           <h1>Attack</h1>
-          <p>{{pokemon.stats.attack}}</p>
+          <p>{{ pokemon.stats.attack }}</p>
         </div>
         <div class="stat">
           <h1>Defense</h1>
-          <p>{{pokemon.stats.defense}}</p>
+          <p>{{ pokemon.stats.defense }}</p>
         </div>
         <div class="stat">
           <h1>Special-Attack</h1>
-          <p>{{pokemon.stats.special_attack}}</p>
+          <p>{{ pokemon.stats.special_attack }}</p>
         </div>
         <div class="stat">
           <h1>Special-Defense</h1>
-          <p>{{pokemon.stats.special_defense}}</p>
+          <p>{{ pokemon.stats.special_defense }}</p>
         </div>
         <div class="stat">
           <h1>Speed</h1>
-          <p>{{pokemon.stats.speed}}</p>
+          <p>{{ pokemon.stats.speed }}</p>
         </div>
       </div>
     </div>
   </div>
   <div v-else>
-    <div class="back-card-line" style="background-color: gray;"></div>
+    <div class="c-loader"></div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
-export default{
+export default {
   props: {
     url: {
       type: String,
-      required: true
+      required: true,
     },
     pokemon_index: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
-  created(){
-    this.fetchPokemonData({index:this.$props.pokemon_index, url:this.$props.url})
-      .then(res => {
-        this.pokemon.name = res.data.name
+  created() {
+    this.fetchPokemonData({
+      index: this.$props.pokemon_index,
+      url: this.$props.url,
+    })
+      .then((res) => {
+        this.pokemon.name = res.data.name;
 
-        this.pokemon.type = res.data.types[0].type.name
-        this.pokemon.types = res.data.types
+        this.pokemon.type = res.data.types[0].type.name;
+        this.pokemon.types = res.data.types;
 
-        this.pokemon.img = res.data.sprites.front_default
-        this.pokemon.stats.hp = res.data.stats[0].base_stat
-        this.pokemon.stats.attack = res.data.stats[1].base_stat
-        this.pokemon.stats.defense = res.data.stats[2].base_stat
-        this.pokemon.stats.special_attack = res.data.stats[3].base_stat
-        this.pokemon.stats.special_defense = res.data.stats[4].base_stat
-        this.pokemon.stats.speed = res.data.stats[5].base_stat
+        this.pokemon.img = res.data.sprites.front_default;
+        this.pokemon.stats.hp = res.data.stats[0].base_stat;
+        this.pokemon.stats.attack = res.data.stats[1].base_stat;
+        this.pokemon.stats.defense = res.data.stats[2].base_stat;
+        this.pokemon.stats.special_attack = res.data.stats[3].base_stat;
+        this.pokemon.stats.special_defense = res.data.stats[4].base_stat;
+        this.pokemon.stats.speed = res.data.stats[5].base_stat;
 
-        this.isFetched = true
+        this.isFetched = true;
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   },
-  data(){
+  data() {
     return {
       isFetched: false,
 
       pokemon: {
-        img:'',
-        name: '',
+        img: "",
+        name: "",
         types: [],
-        type: '',
+        type: "",
         stats: {
-          hp: '',
-          attack: '',
-          defense: '',
-          special_attack:'',
-          special_defense:'',
-          speed:''
-        }
-      }
-    }
+          hp: "",
+          attack: "",
+          defense: "",
+          special_attack: "",
+          special_defense: "",
+          speed: "",
+        },
+      },
+    };
   },
   computed: {
-    ...mapGetters([
-      'getPokemonTypes'
-    ])
+    ...mapGetters(["getPokemonTypes"]),
   },
   methods: {
-    ...mapActions([
-      'fetchPokemonData'
-    ]),
+    ...mapActions(["fetchPokemonData"]),
 
-    pokemonNameUpperCase(name){
-      return name.charAt(0).toUpperCase() + name.slice(1)
-    }
-  }
-}
+    pokemonNameUpperCase(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
 
-  * {
-    color: black;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 500;
-  }
+* {
+  color: black;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+}
 
-  .back-card-line {
-    margin: 10px;
-    width: 220px;
-    height: 280px;
+.back-card-line {
+  margin: 10px;
+  width: 220px;
+  height: 280px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    background-color: #FFFF00;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  background-color: #ffff00;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
 
-    border-radius: 4px;
-    border: 1.5px solid #000000;
-  }
+  border-radius: 4px;
+  border: 1.5px solid #000000;
+}
 
-  .card-fill{
-    width: 209px;
-    height: 266px;
+.card-fill {
+  width: 209px;
+  height: 266px;
 
-    display: flex;
-    flex-direction: column;align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    border-radius: 8px;
-    border: 1.5px solid #000000;
-  }
+  border-radius: 8px;
+  border: 1.5px solid #000000;
+}
 
-  .name-n-power{
-    width: 200px;
-    margin-bottom: 3px;
+.name-n-power {
+  width: 200px;
+  margin-bottom: 3px;
 
-    display: grid;
-    grid-template-columns: 50% 50%;
-    align-items: center;
-    justify-content: space-between;
-  }
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: center;
+  justify-content: space-between;
+}
 
-  .name-n-power h1{
-    margin-left: 10px;
-  }
+.name-n-power h1 {
+  margin-left: 10px;
+}
 
-  .name-n-power .powers h2{
-    margin: -3px;
-    font-size: 12px;
-  }
+.name-n-power .powers h2 {
+  margin: -3px;
+  font-size: 12px;
+}
 
-  .name-n-power .powers{
-    width: 100%;
-    height: 100%;
+.name-n-power .powers {
+  width: 100%;
+  height: 100%;
 
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-    justify-content: flex-start;
-  }
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: flex-start;
+}
 
-  .name-n-power #type-icon{
-    margin-left: 7px;
-    margin-right: 9px;
-    width: 20px;
-    height: 25px;
-  }
+.name-n-power #type-icon {
+  margin-left: 7px;
+  margin-right: 9px;
+  width: 20px;
+  height: 25px;
+}
 
-  .pokemon-display-back-line{
-    width: 179px;
-    height: 115px;
+.pokemon-display-back-line {
+  width: 179px;
+  height: 115px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    background-color: #FFFF00;
-    border-radius: 2px;
-    border: 1.5px solid #000000;
-  }
+  background-color: #ffff00;
+  border-radius: 2px;
+  border: 1.5px solid #000000;
+}
 
-  .pokemon-display{
-    width: 170px;
-    height: 108px;
+.pokemon-display {
+  width: 170px;
+  height: 108px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    background-color: #FFFFFF;
-    border-radius: 2px;
-    border: 1.5px solid #000000;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
-  }
+  background-color: #ffffff;
+  border-radius: 2px;
+  border: 1.5px solid #000000;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+}
 
-  .pokemon-stats{
-    padding: 10px 0px 10px 0px;
+.pokemon-stats {
+  padding: 10px 0px 10px 0px;
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-  .stat{
-    width: 179px;
-    height: 15px;
+.stat {
+  width: 179px;
+  height: 15px;
 
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    margin: 2px 0px 2px 0px;
+  margin: 2px 0px 2px 0px;
 
-    background-color: #FFFFFF;
-    border-radius: 3px;
-    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  }
+  background-color: #ffffff;
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
 
-  .stat h1 {
-    margin-left: 3px;
-    font-size: 11px;
-  }
+.stat h1 {
+  margin-left: 3px;
+  font-size: 11px;
+}
 
-  .stat p {
-    margin-right: 3px;
-    font-size: 11px;
-  }
+.stat p {
+  margin-right: 3px;
+  font-size: 11px;
+}
 
-  .normal{
-    /* background-color: #A8A77A; */
-    background: rgb(168,167,122);
-background: linear-gradient(0deg, rgba(168,167,122,1) 0%, rgba(252,239,239,1) 100%);
-  }
+.normal {
+  /* background-color: #A8A77A; */
+  background: rgb(168, 167, 122);
+  background: linear-gradient(
+    0deg,
+    rgba(168, 167, 122, 1) 0%,
+    rgba(252, 239, 239, 1) 100%
+  );
+}
 
-  .grass{
-   background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #7AC74C;
-  }
+.grass {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #7ac74c;
+}
 
-  .fire{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #EE8130;
-  }
+.fire {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #ee8130;
+}
 
-  .water{
-    background: linear-gradient(180deg, rgba(251, 249, 249, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #6390F0;
-  }
+.water {
+  background: linear-gradient(
+      180deg,
+      rgba(251, 249, 249, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #6390f0;
+}
 
-  .bug{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #A6B91A;
-  }
+.bug {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #a6b91a;
+}
 
-  .poison{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #A33EA1;
-  }
+.poison {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #a33ea1;
+}
 
-  .electric{
-    background: linear-gradient(180deg, rgba(251, 249, 249, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #F7D02C;
-  }
+.electric {
+  background: linear-gradient(
+      180deg,
+      rgba(251, 249, 249, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #f7d02c;
+}
 
-  .ground{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #E2BF65;
-  }
+.ground {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #e2bf65;
+}
 
-  .fairy{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #D685AD;
-  }
+.fairy {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #d685ad;
+}
 
-  .fighting{
-    background: linear-gradient(180deg, rgba(251, 249, 249, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #BBAACC;
-  }
+.fighting {
+  background: linear-gradient(
+      180deg,
+      rgba(251, 249, 249, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #bbaacc;
+}
 
-  .psychic{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #F95587;
-  }
+.psychic {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #f95587;
+}
 
-  .rock{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #B6A136;
-  }
+.rock {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #b6a136;
+}
 
-  .ghost{
-    background: linear-gradient(180deg, rgba(251, 249, 249, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #735797;
-  }
+.ghost {
+  background: linear-gradient(
+      180deg,
+      rgba(251, 249, 249, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #735797;
+}
 
-  .ice{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #96D9D6;
-  }
+.ice {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #96d9d6;
+}
 
-  .flying{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #A98FF3;
-  }
+.flying {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #a98ff3;
+}
 
-  .steel{
-    background: linear-gradient(180deg, rgba(251, 249, 249, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #B7B7CE;
-  }
+.steel {
+  background: linear-gradient(
+      180deg,
+      rgba(251, 249, 249, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #b7b7ce;
+}
 
-  .dark{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #705746;
-  }
+.dark {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #705746;
+}
 
-  .dragon{
-    background: linear-gradient(180deg, rgba(252, 239, 239, 0.2) 0%, rgba(0, 0, 0, 0) 100%), #6F35FC;
-  }
+.dragon {
+  background: linear-gradient(
+      180deg,
+      rgba(252, 239, 239, 0.2) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    #6f35fc;
+}
 
 .c-loader {
   animation: is-rotating 1s infinite;
   border: 6px solid #e5e5e5;
   border-radius: 50%;
   border-top-color: #51d4db;
-  height: 50px;
-  width: 50px;
+  width: 120px;
+  height: 120px;
 }
 
 @keyframes is-rotating {
